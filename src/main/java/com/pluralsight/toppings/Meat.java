73da245c5db37462ef526cfd178b4e7a09cbf7a1
Emre Akarsu <akarsutech@gmail.com>
2024-11-12 10:24:53 -0500

    created the variables, consturctor, and getters for Meat class

--- src/main/java/com/pluralsight/toppings/Meat.java
+++ src/main/java/com/pluralsight/toppings/Meat.java
@@ -1,4 +1,12 @@
 package com.pluralsight.toppings;
 
-public class Meat {
+public class Meat extends Topping{
+    public Meat(String type) {
+        super(type);
+    }
+
+    @Override
+    public double calculatePrice(int size) {
+        return 0;
+    }
 }
