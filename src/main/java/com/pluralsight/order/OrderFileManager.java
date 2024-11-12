466b39193c17100deb97b37bf840ca3221c6eaa2
Emre Akarsu <akarsutech@gmail.com>
2024-11-12 10:41:59 -0500

    created the variables, consturctor, and getters for OrderFileManager class

--- src/main/java/com/pluralsight/order/OrderFileManager.java
+++ src/main/java/com/pluralsight/order/OrderFileManager.java
@@ -1,4 +1,14 @@
 package com.pluralsight.order;
 
 public class OrderFileManager {
+
+    public void writeOrderToFile(String receipt){
+
+
+    }
+
+    public String createReceipt(Order order){
+
+        
+    }
 }
