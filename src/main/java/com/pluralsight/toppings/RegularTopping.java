a59107d7a89482ccc52f4ba2adcf07044a4a1884
Emre Akarsu <akarsutech@gmail.com>
2024-11-12 10:24:17 -0500

    created the variables, consturctor, and getters for RegularTopping class

--- src/main/java/com/pluralsight/toppings/RegularTopping.java
+++ src/main/java/com/pluralsight/toppings/RegularTopping.java
@@ -1,6 +1,14 @@
 package com.pluralsight.toppings;
 
-public class RegularTopping {
+public class RegularTopping extends Topping{
 
 
+    public RegularTopping(String type) {
+        super(type);
+    }
+
+    @Override
+    public double calculatePrice(int size) {
+        return 0;
+    }
 }
