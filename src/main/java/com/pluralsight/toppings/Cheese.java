48f32ccc3706c35eb4cacd0af0b93dee580264cb
Emre Akarsu <akarsutech@gmail.com>
2024-11-12 10:25:26 -0500

    created the variables, consturctor, and getters for Cheese class

--- src/main/java/com/pluralsight/toppings/Cheese.java
+++ src/main/java/com/pluralsight/toppings/Cheese.java
@@ -1,4 +1,12 @@
 package com.pluralsight.toppings;
 
-public class Cheese {
+public class Cheese extends Topping{
+    public Cheese(String type) {
+        super(type);
+    }
+
+    @Override
+    public double calculatePrice(int size) {
+        return 0;
+    }
 }
